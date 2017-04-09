a1c211e699536d297b7334c9b7ac5f3de3dca554
Antoine Mary <antoinee.mary@gmail.com>
2017-04-09 14:54:28 +0200

    Patching done

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,7 @@ RUN apk update && apk upgrade && \
     apk add wget tar ca-certificates gcc make libc-dev libpthread-stubs openssl-dev readline-dev ncurses-dev && \
     update-ca-certificates && \
     addgroup softether && adduser -g 'softether' -G softether -s /sbin/nologin -D -H softether && \
+    mv /assets/entrypoint.sh / && chmod +x /entrypoint.sh && \
 
     # Fetch sources
     wget -O - https://github.com/SoftEtherVPN/SoftEtherVPN/archive/${SOFTETHER_VERSION}.tar.gz | tar xzf - && \
@@ -26,3 +27,6 @@ RUN apk update && apk upgrade && \
     cd .. && rm -rf SoftEtherVPN-${SOFTETHER_VERSION:1} && \
     apk del wget tar ca-certificates gcc make libc-dev libpthread-stubs openssl-dev readline-dev ncurses-dev && \
     rm -rf /var/cache/apk/* /assets
+
+ENTRYPOINT ["/entrypoint.sh"]
+CMD ["vpnserver start"]
--- assets/patchs/00_cipher.sh
+++ assets/patchs/00_cipher.sh
@@ -1,1 +1,35 @@
 #! /bin/sh
+set -e
+
+FILE="src/Cedar/Interop_OpenVPN.h"
+echo "> Patching $FILE <"
+if [ -f $FILE ]; then
+  echo "  - Changing default algorithm -"
+  sed -n 's/"AES-128-CBC"/"AES-256-CBC"/gp' $FILE
+  sed -n 's/"SHA1"/"RMD160"/gp' $FILE
+
+  echo "  - Changing default OpenVPN client option string -"
+  sed -n 's/cipher AES-128-CBC,auth SHA1/cipher AES-256-CBC,auth RMD160/gp' $FILE
+
+  echo "> $FILE Patched <"
+else
+  echo "$FILE don't exist" >&2
+  exit 1
+fi
+
+FILE="src/bin/hamcore/openvpn_sample.ovpn"
+echo "> Patching $FILE <"
+if [ -f $FILE ]; then
+  echo "  - Changing default cipher -"
+  sed -n 's/cipher AES-128-CBC/cipher AES-256-CBC/gp' $FILE
+
+  echo "  - Changing default auth -"
+  sed -n 's/auth SHA1/auth RMD160/gp' $FILE
+
+  echo "> $FILE Patched <"
+else
+  echo "$FILE don't exist" >&2
+  exit 1
+fi
+
+exit 0
