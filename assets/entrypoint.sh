2b58f552c8bbfe05d0990e7d5385ba352b1410c5
Dimitri Gence <dev@dmgnx.net>
2017-06-06 23:15:55 +0200

    fixed persistent configuration issue
    use an alternative directory for configuration and log files and symlink to them not erasing the vpnserver executable when mounting a volume

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -2,6 +2,17 @@
 set -e
 
 if [ "x$1" = 'x/usr/vpnserver/vpnserver' ]; then
+    if [ -f /etc/vpnserver/vpn_server.config ]; then
+        ln -s /etc/vpnserver/vpn_server.config /usr/vpnserver/vpn_server.config
+    fi
+    if [ -d /var/log/vpnserver ]; then
+        mkdir -p /var/log/vpnserver/vpn_server/server_log
+        mkdir -p /var/log/vpnserver/vpn_server/security_log
+        mkdir -p /var/log/vpnserver/vpn_server/packet_log
+        ln -s /var/log/vpnserver/vpn_server/server_log /usr/vpnserver/server_log
+        ln -s /var/log/vpnserver/vpn_server/security_log /usr/vpnserver/security_log
+        ln -s /var/log/vpnserver/vpn_server/packet_log /usr/vpnserver/packet_log
+    fi
     chown -R softether:softether /usr/vpnserver
     setcap 'cap_net_bind_service=+ep' /usr/vpnserver/vpnserver
 
