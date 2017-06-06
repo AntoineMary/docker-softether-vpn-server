b3066d3c4f3dfb14871cc70ac86ada99b57a708b
Dimitri Gence <dev@dmgnx.net>
2017-06-06 23:40:23 +0200

    now initialize only log directories in entrypoint.sh

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -2,17 +2,11 @@
 set -e
 
 if [ "x$1" = 'x/usr/vpnserver/vpnserver' ]; then
-    if [ -f /etc/vpnserver/vpn_server.config ]; then
-        ln -s /etc/vpnserver/vpn_server.config /usr/vpnserver/vpn_server.config
-    fi
-    if [ -d /var/log/vpnserver ]; then
-        mkdir -p /var/log/vpnserver/vpn_server/server_log
-        mkdir -p /var/log/vpnserver/vpn_server/security_log
-        mkdir -p /var/log/vpnserver/vpn_server/packet_log
-        ln -s /var/log/vpnserver/vpn_server/server_log /usr/vpnserver/server_log
-        ln -s /var/log/vpnserver/vpn_server/security_log /usr/vpnserver/security_log
-        ln -s /var/log/vpnserver/vpn_server/packet_log /usr/vpnserver/packet_log
-    fi
+    for d in server_log security_log packet_log;
+    do
+        mkdir -p /var/log/vpnserver/vpn_server/$d
+        ln -s /var/log/vpnserver/vpn_server/server_log /usr/vpnserver/$d
+    done
     chown -R softether:softether /usr/vpnserver
     setcap 'cap_net_bind_service=+ep' /usr/vpnserver/vpnserver
 
